85fb8240f6bbe5cd2277bd5186718d563e58fa09
Rafael Guedes <rafaelmenta@gmail.com>
2018-06-22 23:47:05 -0700

    Better responsiveness on boxscore page

--- src/app/boxscore/team-score/team-score.component.ts
+++ src/app/boxscore/team-score/team-score.component.ts
@@ -1,6 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, INJECTOR, Inject } from '@angular/core';
 import { BoxscorePlayer, BoxscoreTeam, BoxscoreTeamPerformance } from '../../services/game/game.service';
 import { MatTableDataSource } from '@angular/material';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { APP_CONFIG, AppConfig } from '../../app.config';
+
+const ALL_STATS = [
+  'name',
+  'minutes-pg', 'minutes-sg', 'minutes-sf', 'minutes-pf', 'minutes-c',
+  'min', 'fga', 'fta',
+  'orb', 'drb',
+  'ast', 'stl', 'blk', 'to', 'pf', 'pts',
+  'fpm', 'fps', 'fpv',
+];
+
+const ALL_FOOTER_STATS = [
+  'name',
+  'minutes-pg', 'minutes-sg', 'minutes-sf', 'minutes-pf', 'minutes-c',
+  'total', 'score'
+];
 
 @Component({
   selector: 'app-team-score',
@@ -14,22 +31,11 @@ export class TeamScoreComponent implements OnInit {
   @Input() players: BoxscorePlayer[];
 
   datasource = new MatTableDataSource<BoxscorePlayer>();
-  displayedColumns = [
-    'name',
-    'minutes-pg', 'minutes-sg', 'minutes-sf', 'minutes-pf', 'minutes-c',
-    'min', 'fga', 'fta',
-    'orb', 'drb',
-    'ast', 'stl', 'blk', 'to', 'pf', 'pts',
-    'fpm', 'fps', 'fpv',
-  ];
+  displayedColumns = ALL_STATS;
 
   dnpColumns = ['name', 'dnp'];
 
-  footerColumns = [
-    'name',
-    'minutes-pg', 'minutes-sg', 'minutes-sf', 'minutes-pf', 'minutes-c',
-    'total', 'score'
-  ];
+  footerColumns = ALL_FOOTER_STATS;
 
   isValidGame = (index: number, stat: BoxscorePlayer) => stat.performance.minutes > 0;
 
@@ -43,10 +49,29 @@ export class TeamScoreComponent implements OnInit {
     }
   }
 
-  constructor() { }
+  constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
+    private breakpoint$: BreakpointObserver) { }
 
   ngOnInit() {
     this.datasource.data = this.players;
+    this.breakpoint$.observe(this.config.LARGE_MOBILE_QUERY).subscribe(res => {
+      if (res.matches) {
+        this.displayedColumns = [
+          'name',
+          'trb',
+          'ast', 'stl', 'pts',
+          'fps', 'fpv',
+        ];
+
+        this.footerColumns = [
+          'total', 'score'
+        ];
+      } else {
+        this.displayedColumns = ALL_STATS;
+        this.footerColumns = ALL_FOOTER_STATS;
+      }
+    });
   }
 
 }
