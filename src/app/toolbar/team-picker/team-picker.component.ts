dfdf11ecdc484d41a85b9af28c71af7385c95e3d
Rafael Guedes <rafaelmenta@gmail.com>
2018-07-01 12:21:58 -0700

    Add event tracking for change default

--- src/app/toolbar/team-picker/team-picker.component.ts
+++ src/app/toolbar/team-picker/team-picker.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { UserTeam, UserService, User } from '../../services/user.service';
 import { TeamService } from '../../services/team.service';
+import { Angulartics2 } from 'angulartics2';
 
 @Component({
   selector: 'app-team-picker',
@@ -12,10 +13,20 @@ export class TeamPickerComponent implements OnInit {
   teams: UserTeam[];
   defaultTeam: UserTeam;
 
-  constructor(private userService: UserService, private teamService: TeamService) { }
+  constructor(
+    private userService: UserService,
+    private angulartics: Angulartics2,
+    private teamService: TeamService) { }
 
   selectTeam(team: UserTeam) {
-    this.userService.changeDefaultTeam(team).subscribe();
+    this.userService.changeDefaultTeam(team).subscribe(() => {
+      this.angulartics.eventTrack.next({
+        action: 'change-default',
+        properties: {
+          category: 'user',
+        }
+      });
+    });
   }
 
   ngOnInit() {
