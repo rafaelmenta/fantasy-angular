6210e668cbac3657ebdf81d165a6592c5afcc26d
Rafael Guedes <rafaelmenta@gmail.com>
2018-07-18 21:36:27 -0700

    Better default states for standings

--- src/app/standings/standings/standings.component.ts
+++ src/app/standings/standings/standings.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material';
 import { Team, TeamService } from '../../services/team.service';
-import { UserService } from '../../services/user.service';
+import { UserService, User } from '../../services/user.service';
 import { LeagueService } from '../../services/league/league.service';
 import { flatTeams } from '../../../lib/utils';
 import { Title } from '@angular/platform-browser';
-import { League, Conference } from '../../../typings';
+import { League, Conference, Division } from '../../../typings';
 
 @Component({
   selector: 'app-standings',
@@ -14,30 +14,66 @@ import { League, Conference } from '../../../typings';
 })
 export class StandingsComponent implements OnInit {
 
-  @ViewChild('group') group: ElementRef;
-  @ViewChild('subgroup') subgroup: ElementRef;
+  @ViewChild('group') group: any;
 
-  public teams;
   private league: League;
-  private conferences: Conference[];
 
-  filterTeams(group, subgroup) {
+  teams;
 
-    group = group || this.group;
-    subgroup = subgroup || this.subgroup;
+  conferences: Conference[];
+  divisions: Division[];
 
+  conference: string;
+  division: string;
+
+  filterTeams() {
     let teams = flatTeams(this.league);
 
-    if (group.value === 'division') {
-      teams = teams.filter(team => team.division === subgroup.value);
+    if (this.group.value === 'division') {
+      teams = teams.filter(team => team.division === this.division);
     }
 
-    if (group.value === 'conference') {
-      teams = teams.filter(team => team.conference === subgroup.value);
+    if (this.group.value === 'conference') {
+      teams = teams.filter(team => team.conference === this.conference);
     }
 
     this.teams = teams;
     return teams;
+  }
+
+  getDivisions(conferences: Conference[]) {
+    return conferences.map(conference => conference.divisions).reduce((soFar, divisions) => {
+      return [...soFar, ...divisions];
+    }, []);
+  }
+
+  initDefaults() {
+    this.group = { value: 'division' };
+    this.division = this.divisions[0].name;
+    this.conference = this.conferences[0].name;
+  }
+
+  onLeagueLoad(league?: League) {
+    if (!league) {
+      return;
+    }
+
+    this.league = league;
+    this.teams = flatTeams(league);
+
+    this.conferences = league.conferences;
+    this.divisions = this.getDivisions(this.conferences);
+
+    this.initDefaults();
+    this.filterTeams();
+  }
+
+  onUserLoad(user?: User) {
+    if (user) {
+      const team = this.teamService.getDefaultTeam(user.teams);
+      const id = team.team.division.conference.league.id_league;
+      this.leagueService.getUserLeague(id).subscribe(this.onLeagueLoad.bind(this));
+    }
   }
 
   constructor(
@@ -48,24 +84,7 @@ export class StandingsComponent implements OnInit {
 
   ngOnInit() {
     this.title.setTitle(`Superliga - Classificação`);
-    this.userService.user.subscribe(user => {
-      if (user) {
-        const team = this.teamService.getDefaultTeam(user.teams);
-        const id = team.team.division.conference.league.id_league;
-        this.leagueService.getUserLeague(id).subscribe((res: League) => {
-          if (!res) {
-            return;
-          }
-          this.league = res;
-          this.teams = flatTeams(res);
-          this.conferences = res.conferences;
-
-          const group = { value: 'division' };
-          const subgroup = { value: this.league.conferences[0].divisions[0].name };
-          this.filterTeams(group, subgroup);
-        });
-      }
-    });
+    this.userService.user.subscribe(this.onUserLoad.bind(this));
   }
 
 }
