38f5c6f31b533b181726a2b476a2e4dafc3c1e10
Rafael Guedes <rafaelmenta@gmail.com>
2018-07-19 23:17:39 -0700

    Use lookup on draft, change card title

--- src/app/common/lookup/lookup.component.ts
+++ src/app/common/lookup/lookup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Team } from '../../services/team.service';
+import { Team, TeamLookup } from '../../services/team.service';
 import { Player } from '../../services/player/player.service';
 
 @Component({
@@ -9,12 +9,16 @@ import { Player } from '../../services/player/player.service';
 })
 export class LookupComponent implements OnInit {
 
-  @Input() element: Player | Team;
+  @Input() element: Player | TeamLookup;
 
   constructor() { }
 
   get isPlayer() {
     return this.element && (this.element as Player).id_player !== 0;
+  }
+
+  get isTeam() {
+    return this.element && (this.element as TeamLookup).id_sl !== 0;
   }
 
   ngOnInit() {
